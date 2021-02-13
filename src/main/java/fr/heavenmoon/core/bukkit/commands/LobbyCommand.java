d84afd40e0669e2b3323744fb0fc1a2b994abbbb
DAYELA <damien.ayela@gmail.com>
2021-02-13 20:48:38 +0100

    :bug: fix lobby command

--- src/main/java/fr/heavenmoon/core/bukkit/commands/LobbyCommand.java
+++ src/main/java/fr/heavenmoon/core/bukkit/commands/LobbyCommand.java
@@ -2,10 +2,13 @@ package fr.heavenmoon.core.bukkit.commands;
 
 import fr.heavenmoon.core.bukkit.MoonBukkitCore;
 import fr.heavenmoon.core.bukkit.format.Message;
+import fr.heavenmoon.core.common.format.message.MessageType;
 import fr.heavenmoon.persistanceapi.customs.player.CustomPlayer;
 import fr.heavenmoon.persistanceapi.customs.player.data.RankList;
 import fr.heavenmoon.persistanceapi.PersistanceManager;
 import fr.heavenmoon.core.common.format.message.PrefixType;
+import fr.heavenmoon.persistanceapi.customs.server.CustomServer;
+import fr.heavenmoon.persistanceapi.customs.server.ServerType;
 import fr.heavenmoon.persistanceapi.managers.redis.RedisPublisher;
 import fr.heavenmoon.persistanceapi.managers.redis.RedisTarget;
 import org.bukkit.Bukkit;
@@ -14,6 +17,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.Random;
 
 public class LobbyCommand implements CommandExecutor
 {
@@ -33,24 +39,36 @@ public class LobbyCommand implements CommandExecutor
 			return false;
 		Player player = (Player) sender;
 		CustomPlayer customPlayer = persistanceManager.getPlayerManager().getCustomPlayer(player.getUniqueId());
-		if (args.length == 0)
+		if (args.length != 0)
 		{
-			if (customPlayer.getServerName().equals("lobby01"))
-			{
-				Location spawn = new Location(Bukkit.getWorld("lobby"), 44.5D, 88.0D, 11.5D, 0.0F, 0.0F);
-				player.teleport(spawn);
-				return false;
-			}
-			if (customPlayer.hasPermission(RankList.MODERATEUR))
-			{
-				new RedisPublisher(persistanceManager, "Connect").setArguments(customPlayer.getName(), "lobby01")
-				                                                  .publish(new RedisTarget(RedisTarget.RedisTargetType.PROXY));
-				return true;
-			}
-			new Message(PrefixType.SERVER, "Vous allez être téléporté au lobby.").send(player);
-			new Message(PrefixType.SERVER, "Veillez ne pas vous déplacer durant la téléportation").send(player);
-			plugin.getTpManager().startCooldownTp(customPlayer, player.getLocation(), 5);
+			new Message(MessageType.SYNTAXE.getContent().replace("%syntax%", "/lobby")).send(customPlayer);
+			return false;
 		}
+		if (!customPlayer.canTp() && !customPlayer.hasPermission(RankList.MODERATEUR))
+		{
+			new Message(PrefixType.ERROR, "Vous ne pouvez pas vous téléporter maintenant.");
+			return false;
+		}
+		List<CustomServer> lobbyList = persistanceManager.getServerManager().getHubs();
+		Random random = new Random();
+		String serverName = lobbyList.get(random.nextInt(lobbyList.size())).getName();
+		CustomServer current = persistanceManager.getServerManager().getCustomServer(customPlayer.getServerName());
+		if (current.getType().equals(ServerType.LOBBY))
+		{
+			player.performCommand("spawn");
+			return true;
+		}
+
+		if (customPlayer.hasPermission(RankList.MODERATEUR))
+		{
+			new RedisPublisher(persistanceManager, "Connect").setArguments(customPlayer.getName(), serverName)
+			                                                 .publish(new RedisTarget(RedisTarget.RedisTargetType.PROXY));
+			return true;
+		}
+		new Message(PrefixType.SERVER, "Vous allez être téléporté au lobby.").send(player);
+		new Message(PrefixType.SERVER, "Veuillez ne pas vous déplacer durant la téléportation").send(player);
+		plugin.getTpManager().startCooldownTp(customPlayer, player.getLocation(), 5, serverName);
+
 		return false;
 	}
 }
--- src/main/java/fr/heavenmoon/core/bukkit/tp/TpManager.java
+++ src/main/java/fr/heavenmoon/core/bukkit/tp/TpManager.java
@@ -17,50 +17,60 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TpManager {
-
-    private final MoonBukkitCore plugin;
-    private final PersistanceManager persistanceManager;
-    private Map<CustomPlayer, Integer> tpCooldowns;
-
-    public TpManager(MoonBukkitCore plugin) {
-        this.plugin = plugin;
-        this.persistanceManager = plugin.getCommons().getPersistanceManager();
-        this.tpCooldowns = new HashMap<>();
-    }
-
-    public void startCooldownTp(CustomPlayer customPlayer, Location loc, int seconds) {
-        if (!customPlayer.canTp()) {
-            new Message(PrefixType.ERROR, "Vous ne pouvez pas vous tmaintenant !");
-            return;
-        }
-        LambdaWrapper<Integer> cooldownTime = new LambdaWrapper(seconds);
-        customPlayer.setCanTp(false);
-        int scheduler = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
-            Player player = Bukkit.getPlayer(customPlayer.getName());
-            if (player != null) {
-                new ActionbarBuilder( ChatColor.GRAY + "Téléportation dans " + ChatColor.LIGHT_PURPLE + cooldownTime.getData() + ChatColor.GRAY + " seconde(s)...", 2).send(player);
-            }
-            cooldownTime.setData(cooldownTime.getData() - 1);
-            if (cooldownTime.getData() == 0) {
-                customPlayer.setCanTp(true);
-                Bukkit.getScheduler().cancelTask(tpCooldowns.get(customPlayer));
-                new RedisPublisher(persistanceManager, "Connect").setArguments(customPlayer.getName(), "lobby01").publish(new RedisTarget(RedisTarget.RedisTargetType.PROXY));
-                return;
-            }
-            if (player != null) {
-                if (player.getLocation().getX() != loc.getX() || player.getPlayer().getLocation().getY() != loc.getY() || player.getLocation().getZ() != loc.getZ()) {
-                    new Message(PrefixType.ERROR, "Vous vous êtes déplacé ce qui a annulé votre téléportation.").send(player);
-                    customPlayer.setCanTp(true);
-                    Bukkit.getScheduler().cancelTask(tpCooldowns.get(customPlayer));
-                }
-            }
-
-        }, 0L, 20L);
-        tpCooldowns.put(customPlayer, scheduler);
-    }
-
-    public Map<CustomPlayer, Integer> getTpCooldowns() {
-        return tpCooldowns;
-    }
+public class TpManager
+{
+	
+	private final MoonBukkitCore plugin;
+	private final PersistanceManager persistanceManager;
+	private Map<CustomPlayer, Integer> tpCooldowns;
+	
+	public TpManager(MoonBukkitCore plugin)
+	{
+		this.plugin = plugin;
+		this.persistanceManager = plugin.getCommons().getPersistanceManager();
+		this.tpCooldowns = new HashMap<>();
+	}
+	
+	public void startCooldownTp(CustomPlayer customPlayer, Location loc, int seconds, String serverName)
+	{
+		
+		LambdaWrapper<Integer> cooldownTime = new LambdaWrapper(seconds);
+		customPlayer.setCanTp(false);
+		int scheduler = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () ->
+		{
+			Player player = Bukkit.getPlayer(customPlayer.getName());
+			if (player != null)
+			{
+				new ActionbarBuilder(
+						ChatColor.GRAY + "Téléportation dans " + ChatColor.LIGHT_PURPLE + cooldownTime.getData() + ChatColor.GRAY +
+								" seconde(s)...", 2).send(player);
+			}
+			cooldownTime.setData(cooldownTime.getData() - 1);
+			if (cooldownTime.getData() == 0)
+			{
+				customPlayer.setCanTp(true);
+				Bukkit.getScheduler().cancelTask(tpCooldowns.get(customPlayer));
+				new RedisPublisher(persistanceManager, "Connect").setArguments(customPlayer.getName(), serverName)
+				                                                 .publish(new RedisTarget(RedisTarget.RedisTargetType.PROXY));
+				return;
+			}
+			if (player != null)
+			{
+				if (player.getLocation().getX() != loc.getX() || player.getPlayer().getLocation().getY() != loc.getY() ||
+						player.getLocation().getZ() != loc.getZ())
+				{
+					new Message(PrefixType.ERROR, "Vous vous êtes déplacé ce qui a annulé votre téléportation.").send(player);
+					customPlayer.setCanTp(true);
+					Bukkit.getScheduler().cancelTask(tpCooldowns.get(customPlayer));
+				}
+			}
+			
+		}, 0L, 20L);
+		tpCooldowns.put(customPlayer, scheduler);
+	}
+	
+	public Map<CustomPlayer, Integer> getTpCooldowns()
+	{
+		return tpCooldowns;
+	}
 }
