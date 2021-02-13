33ffc809defe6d455e3633d37541c4f0b82ea4c9
DAYELA <damien.ayela@gmail.com>
2021-02-13 20:23:40 +0100

    :bug: fix 403 request error when fetching playerdb uuid api

--- src/main/java/fr/heavenmoon/core/bukkit/utils/BUniqueID.java
+++ src/main/java/fr/heavenmoon/core/bukkit/utils/BUniqueID.java
@@ -1,5 +1,14 @@
 package fr.heavenmoon.core.bukkit.utils;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.json.simple.JSONObject;
@@ -17,9 +26,10 @@ import java.util.UUID;
 
 public class BUniqueID
 {
+	private static final CloseableHttpClient httpClient = HttpClients.createDefault();
 	
-	private static String PROFILE_URL = "https://api.mojang.com/users/profiles/minecraft/";
-	private static String PLAYERDB = "http://playerdb.co/api/player/minecraft/";
+	private static final String PROFILE_URL = "https://api.mojang.com/users/profiles/minecraft/";
+	private static final String PLAYERDB = "http://playerdb.co/api/player/minecraft/";
 	private static JSONParser JSON_PARSER = new JSONParser();
 	
 	public static String generate()
@@ -32,30 +42,66 @@ public class BUniqueID
 		List<String> APIS = Arrays.asList(PROFILE_URL, PLAYERDB);
 		Random random = new Random();
 		String api = APIS.get(random.nextInt(APIS.size()));
-		try
+		switch (api)
 		{
-			URL url = new URL(api + name);
-			
-			JSONObject json;
-			
-			try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream())))
-			{
-				json = (JSONObject) JSON_PARSER.parse(in);
-			}
-			catch (ParseException e)
-			{
+			case PLAYERDB:
+				HttpGet request = new HttpGet(PLAYERDB + "DAYELA");
+				
+				// add request headers
+				request.addHeader("custom-key", "mkyong");
+				request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+				
+				try (CloseableHttpResponse response = httpClient.execute(request))
+				{
+					
+					HttpEntity entity = response.getEntity();
+					Header headers = entity.getContentType();
+					
+					if (entity != null) {
+						// return it as a String
+						String result = EntityUtils.toString(entity);
+						JSONParser parser = new JSONParser();
+						JSONObject jsonObject = (JSONObject) parser.parse(result);
+						JSONObject data = (JSONObject) parser.parse(jsonObject.get("data").toString());
+						JSONObject player = (JSONObject) parser.parse(data.get("player").toString());
+						return player.get("id").toString();
+					}
+					
+				}
+				catch (IOException | ParseException e)
+				{
+					e.printStackTrace();
+				}
+				break;
+			case PROFILE_URL:
+				try
+				{
+					URL url = new URL(api + name);
+					
+					JSONObject json;
+					
+					try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream())))
+					{
+						json = (JSONObject) JSON_PARSER.parse(in);
+					}
+					catch (ParseException e)
+					{
+						return null;
+					}
+					String id = (String) json.get("id");
+					if (id != null)
+						return id.substring(0, 8) + "-" + id.substring(8, 12) + "-" + id.substring(12, 16) + "-" + id.substring(16, 20) + "-" +
+								id.substring(20, 32);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			default:
 				return null;
-			}
-			String id = (String) json.get("id");
-			if (id != null)
-				return id.substring(0, 8) + "-" + id.substring(8, 12) + "-" + id.substring(12, 16) + "-" + id.substring(16, 20) + "-" +
-						id.substring(20, 32);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
 		}
 		return null;
+		
 	}
 	
 	public static String generate(String name)
